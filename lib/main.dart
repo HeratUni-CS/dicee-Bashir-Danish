fc347027e6dd1d5b7aaac18a4cd9ec40ffd38e69
Bashir-Danish <bashirdanish124@gmail.com>
2023-04-30 16:49:45 +0430

    Dice images added

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,39 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
           backgroundColor: Colors.red,
           title: Text('Dice'),
         ),
-        body: MyApp(),
+        body: const Dice(),
       ),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class Dice extends StatelessWidget {
+  const Dice({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: const [
+          Expanded(
+              child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Image(image: AssetImage('images/dice1.png')),
+          )),
+          Expanded(
+              child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Image(image: AssetImage('images/dice2.png')),
+          )),
+        ],
+      ),
+    );
   }
 }
